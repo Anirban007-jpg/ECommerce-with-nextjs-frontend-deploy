a3646260658695da10b7817f35c8cf180ef4f0f6
Anirban Banerjee <abanerjee763@outlook.com>
2021-07-02 20:45:42 +0530

    Little changes

--- pages/_document.js
+++ pages/_document.js
@@ -28,8 +28,6 @@ class MyDocument extends Document {
               <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossOrigin="anonymous"></script>
               <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-              <script src="https://cdn.lr-ingest.io/LogRocket.min.js" crossOrigin="anonymous"></script>
-              <script>window.LogRocket && window.LogRocket.init('lz7et9/gdsggds');</script>
               <script src="//cdn.muicss.com/mui-0.10.3/js/mui.min.js"></script>
         </body>
       </Html>
