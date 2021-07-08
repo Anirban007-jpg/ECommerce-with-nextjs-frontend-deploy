f46c9dc3799f1ae26363b73a708692d8cfca852e
Anirban Banerjee <abanerjee763@outlook.com>
2021-07-08 23:39:48 +0530

    Closes #13

--- components/forms/FileUpload.js
+++ components/forms/FileUpload.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Resizer from 'react-image-file-resizer';
 
 const FileUpload = () => {
 
     const fileUploadAndResize = (e) => {
         // console.log(e.target.files);
         // resize the images one by one
+        let files = e.target.files;
+        if (files){
+            for ( let i = 0; i < files.length; i++){
+                Resizer.imageFileResizer(files[i], 720, 720 , 'JPEG', 100, 0, (uri) => {
+                    console.log(uri);
+                },  "base64");
+                   
+            }
+        }
         // send back the images to the server to upload to cloudinary
         // set url to images array in the ProductCreate Component
     }
